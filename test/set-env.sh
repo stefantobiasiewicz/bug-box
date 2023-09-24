088bf2444cf978e7a66cfe49d23af9af6a36911d
stefantobiasiewicz <stefantobiasiewicz@gmail.com>
2023-09-24 19:59:36 +0200

    more test scripts

---
+++ test/set-env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export BLOB_STORAGE_URL="https://example.blob.core.windows.net"
+export BLOB_STORAGE_ACCESS_KEY="your_access_key"
+export BLOB_STORAGE_SECRET_KEY="your_secret_key"
+export BLOB_STORAGE_BUCKET="your_bucket_name"
+export BLOB_STORAGE_PATH="your_blob_path"
+export MQTT_BROKER_URL="mqtt://mqtt.example.com"
+export MQTT_BROKER_USERNAME="your_mqtt_username"
+export MQTT_BROKER_PASSWORD="your_mqtt_password"
+export MQTT_TOPIC="your_mqtt_topic"
