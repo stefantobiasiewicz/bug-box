06652e5b8348f0c6966579eeef482d5acb784e73
stefantobiasiewicz <stefantobiasiewicz@gmail.com>
2023-09-24 20:23:56 +0200

    installs scripts update

--- installs/install.sh
+++ installs/install.sh
@@ -5,15 +5,15 @@ git clone https://github.com/minio/minio-py
 cd minio-py
 sudo python setup.py install
 cd ..
-rmdir minio-py
+sudo rm -rf minio-py
 
-echo "picamera"
+echo "installing: picamera"
 sudo pip3 install picamera
 
-echo "rpi_ws281x adafruit-circuitpython-neopixel"
+echo "installing: rpi_ws281x adafruit-circuitpython-neopixel"
 sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel
 
-echo "adafruit-circuitpython-sht4x"
+echo "installing: adafruit-circuitpython-sht4x"
 sudo pip3 install adafruit-circuitpython-sht4x
 
 echo "testing hardware - pls look for"
