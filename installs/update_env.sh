66a54d9fd083082419283cb5d39277a8cd7e8c39
stefantobiasiewicz <stefantobiasiewicz@gmail.com>
2023-09-29 14:07:49 +0200

    env management

--- installs/update_env.sh
+++ installs/update_env.sh
@@ -1,20 +1,52 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <environment_variable_name> <new_value>"
-    exit 1
-fi
-
-variable_name="$1"
-new_value="$2"
-
+# Configuration file path
 config_file="/opt/bug-box/set-env.sh"
 
-if ! grep -q "^export $variable_name=" "$config_file"; then
-    echo "Environment variable '$variable_name' does not exist in the configuration file."
-    exit 1
-fi
+# Function to read and set a variable
+set_variable() {
+    echo -n "Enter a new value for $1: "
+    read new_value
+    # Set the variable in the configuration file
+    sed -i "s/^export $1=.*/export $1=\"$new_value\"/" "$config_file"
+    echo "Updated variable $1 to \"$new_value\"."
+}
 
-sed -i "s/^export $variable_name=.*/export $variable_name=\"$new_value\"/" "$config_file"
+# Interactive menu
+while true; do
+    clear
+    echo "1. Set BOX_NAME"
+    echo "2. Set LED_PIXEL_COUNT"
+    echo "3. Set IMAGE_CRON"
+    echo "4. Set ENV_CRON"
+    echo "5. Set BLOB_STORAGE_URL"
+    echo "6. Set BLOB_STORAGE_ACCESS_KEY"
+    echo "7. Set BLOB_STORAGE_SECRET_KEY"
+    echo "8. Set BLOB_STORAGE_BUCKET"
+    echo "9. Set MQTT_BROKER_URL"
+    echo "10. Set MQTT_BROKER_PORT"
+    echo "11. Set MQTT_BROKER_USERNAME"
+    echo "12. Set MQTT_BROKER_PASSWORD"
+    echo "13. Set MQTT_TOPIC"
+    echo "0. Exit"
 
-echo "Updated environment variable '$variable_name' to '$new_value' in the configuration file."
+    read -p "Choose an option: " option
+    case $option in
+        1) set_variable "BOX_NAME";;
+        2) set_variable "LED_PIXEL_COUNT";;
+        3) set_variable "IMAGE_CRON";;
+        4) set_variable "ENV_CRON";;
+        5) set_variable "BLOB_STORAGE_URL";;
+        6) set_variable "BLOB_STORAGE_ACCESS_KEY";;
+        7) set_variable "BLOB_STORAGE_SECRET_KEY";;
+        8) set_variable "BLOB_STORAGE_BUCKET";;
+        9) set_variable "MQTT_BROKER_URL";;
+        10) set_variable "MQTT_BROKER_PORT";;
+        11) set_variable "MQTT_BROKER_USERNAME";;
+        12) set_variable "MQTT_BROKER_PASSWORD";;
+        13) set_variable "MQTT_TOPIC";;
+        0) break;;
+        *) echo "Invalid option";;
+    esac
+    read -p "Press Enter to continue..."
+done
